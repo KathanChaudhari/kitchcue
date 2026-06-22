6189fcd082107ffb1bc5635606f71d7fed883471
kathan <kathanc2651@gmail.com>
2026-06-22 18:06:43 +0530

    cleanup

--- components/home/NearExpiryCard.tsx
+++
@@ -1,5 +0,0 @@
-import { EmptyState } from "@/components/global/EmptyState";
-
-export function NearExpiryCard() {
-  return <EmptyState title="Near expiry" description="Expiry tracking will appear here once items have dates." />;
-}
